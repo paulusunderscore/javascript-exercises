62200d087a61f3f608aed45b1aed7757ce126d0b
paulus <paulusojak@gmail.com>
2024-02-17 02:11:20 +0700

    Add attempt for exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let textContainer = "";
+    for (let i = 0; i < text.length; i++){
+        textContainer += text.slice(-1);
+        text = text.slice(0, -1);
+    }
+    return textContainer;
 };
 
 // Do not edit below this line
