f74bb318d95f92392aeec475b7217c9788483a59
paulus <paulusojak@gmail.com>
2024-02-17 01:55:37 +0700

    Add solutions for exercise 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(text, num) {
+    let finalText ="";
+    if(num < 0){
+        finalText = "ERROR";
+    }
+    for(let i = 0; i < num; i++){
+        finalText += text;
+    }
+    return finalText;
 };
 
 // Do not edit below this line
