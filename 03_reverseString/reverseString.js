ba802ccc9d03b0ab71b70049e53ff1c49710a0d2
paulus <paulusojak@gmail.com>
2024-02-19 17:48:09 +0700

    Add solution for exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,10 +1,12 @@
 const reverseString = function(text) {
-    let textContainer = "";
-    for (let i = 0; i < text.length; i++){
+    /*let textContainer = "";
+    let n = text.length;
+    for (let i = 0; i < n; i++){
         textContainer += text.slice(-1);
         text = text.slice(0, -1);
     }
-    return textContainer;
+    return textContainer;*/
+    return text.split("").reverse().join("");
 };
 
 // Do not edit below this line
