1404b8e20f0e91a3432af2587b88dd482e32c6bb
paulus <paulusojak@gmail.com>
2024-02-19 19:41:58 +0700

    Add solution for exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
 const removeFromArray = function() {
-
+    let array = [];
+    for (let arg = 1; arg < arguments.length; arg++){
+        for (let i = 0; i < arguments[0].length; i++){
+            array = arguments[0];
+            if (arguments[arg] === array[i]){
+                array.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
