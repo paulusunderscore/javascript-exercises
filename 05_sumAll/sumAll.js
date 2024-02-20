0c4a68f7e635ec37cf064579ccc84eb85c75ca3b
paulus <paulusojak@gmail.com>
2024-02-20 20:43:25 +0700

    Add solution to exercise 05, 06, and 07

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(a, n) {
+    let container = 0;
+    if (n < a){
+        container = a;
+        a = n;
+        n = container;
+    }
+    else if (a < 0 || n < 0 || typeof(a) === "string" ||
+    typeof(n) === "string" || isNaN(a) || isNaN(n)){
+        return "ERROR";
+    }
+    return ((n/2)*(2*a + (n-1)));
 };
 
 // Do not edit below this line
