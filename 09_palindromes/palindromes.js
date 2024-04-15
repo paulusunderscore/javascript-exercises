c716a1d59e1da53e1a6363323f76add028891a5d
paulus <paulusojak@gmail.com>
2024-04-16 02:56:39 +0700

    Add solution to question number 10

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,9 +1,6 @@
 const palindromes = function (text) {
     let newText = text.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '');
     let noSpace = newText.toLowerCase().replace(/\s/g,'');
-    console.log('noSpace: ' + noSpace);
-    console.log('A : ' + noSpace.toLowerCase());
-    console.log('B : ' + noSpace.toLowerCase().split('').reverse().join(''));
     if(noSpace===noSpace.split('').reverse().join('')){
         return true;
     }
