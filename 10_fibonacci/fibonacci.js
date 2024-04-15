cb72977f847ae50db20b090f366665b4edbd7360
paulus <paulusojak@gmail.com>
2024-04-16 02:56:41 +0700

    Add solution to fibonacci question

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    let arr = [];
+    const num = Number(input);
+    if(num < 0){
+        return 'OOPS';
+    }
+    if(num === 0){
+        return 0;
+    }
+    for(let i = 0; i < num; i++){
+        if(!arr[1]){
+            arr[i] = 1;
+        }
+        arr = arr.concat(arr[i] + arr[i-1]);
+    }
+    console.log('arr : '+arr);
+    return arr[arr.length - 2];
 };
 
 // Do not edit below this line
